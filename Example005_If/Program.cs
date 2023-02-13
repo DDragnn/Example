fcaeba3ca044c4425c82a7c562c80ddcaa3aa5f0
Yuri <drogn@inbox.ru>
2023-02-13 11:45:45 +0300

    Example005_If

--- Example005_If/Program.cs
+++ Example005_If/Program.cs
@@ -1,12 +1,2 @@
-﻿Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine();
-
-if(username.ToLower() =="маша")
-{
-    Console.WriteLine("Ура, Это же МАША!");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
